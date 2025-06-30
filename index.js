b7f9e292021cb26676f73764f87a8d47f81784db
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-06-30 16:44:52 +0600

    jwt

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express()
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ObjectId } = require('mongodb')
-const { ServerApiVersion } = require('mongodb')
+const { ObjectId } = require('mongodb')
 
 
 
@@ -28,37 +27,36 @@ const client = new MongoClient(uri, {
 async function run () {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect()
+    // await client.connect()
 
     const doctorsCollection = client.db('hashi').collection('doctors')
     const userCollection = client.db('hashi').collection('users')
     const appointmentCollection = client.db('hashi').collection('appointment')
     const reviewsCollection = client.db('hashi').collection('reviews')
 
-        // Jwt related api
+      // jwt related api
     app.post('/jwt', async (req, res) => {
-      const user = req.body
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1hr'
-      })
-      res.send({ token })
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
     })
 
-    // middleware
+    // middlewares 
     const verifyToken = (req, res, next) => {
-      // console.log('inside token', req.headers.authorization)
+      console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'forbidded access' })
+        return res.status(401).send({ message: 'unauthorized access' });
       }
-      const token = req.headers.authorization.split(' ')[1]
+      const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: 'forbidden access' })
+          return res.status(401).send({ message: 'unauthorized access' })
         }
-        req.decoded = decoded
-        next()
+        req.decoded = decoded;
+        next();
       })
     }
+
 
     app.get('/doctors', async (req, res) => {
       const result = await doctorsCollection.find().toArray()
@@ -66,6 +64,10 @@ async function run () {
     })
 
     // user related apis
+        app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
     app.post('/users', async (req, res) => {
       const user = req.body
       const query = { email: user.email }
@@ -76,6 +78,82 @@ async function run () {
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
+
+  app.get('/users/role/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'Unauthorized access' })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+
+      // const admin = user.role === 'admin'
+      const role = user.role
+
+      res.send({ role })
+    })
+
+    app.patch('/users/role/:id', async (req, res) => {
+      const id = req.params.id
+      const { role } = req.body
+      const allowedRoles = ['tourist', 'guide', 'admin']
+      if (!allowedRoles.includes(role)) {
+        return res.status(400).send({ message: 'Invalid role specified.' })
+      }
+      const filter = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: { role: role }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    app.patch(
+      '/users/guide/:id',
+      // verifyToken,
+      // verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id
+        const { role } = req.body
+        const filter = { _id: new ObjectId(id) }
+        const updatedDoc = {
+          $set: { role }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+      }
+    )
+
+    app.get('/users/guides', async (req, res) => {
+      const guides = await userCollection.find({ role: 'guide' }).toArray()
+      res.send(guides)
+    })
+
+    app.get('/user', async (req, res) => {
+      const email = req.query.email
+      const result = await userCollection.findOne({ email })
+      res.send(result)
+    })
+
+    app.put('/users/:id', async (req, res) => {
+      const { id } = req.params
+      const updateData = req.body
+      const result = await userCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      )
+      res.send(result)
+    })
+
+    app.delete('/users/:id',  async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
 
         // appointment related api
     app.post('/appointment', async (req, res) => {
@@ -117,11 +195,19 @@ async function run () {
 
 
    // reviews related api
-    app.post('/reviews', async (req, res) => {
-      const story = req.body
-      const result = await reviewsCollection.insertOne(story)
-      res.send(result)
-    })
+
+   // POST route to add review
+app.post("/reviews", async (req, res) => {
+    const { review, date } = req.body;
+    const result = await reviewsCollection.insertOne({ review, date });
+    res.send(result); 
+});
+
+    // app.post('/reviews', async (req, res) => {
+    //   const story = req.body
+    //   const result = await reviewsCollection.insertOne(story)
+    //   res.send(result)
+    // })
 
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray()
