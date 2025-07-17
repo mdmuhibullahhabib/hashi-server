bbcf71a7a460342a3962c98d9547df02698a1048
mdmuhibullahhabib <mdmuhibullahhabib6348@gmail.com>
2025-07-17 19:16:59 +0600

    token

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ async function run () {
 
     app.patch(
       '/users/guide/:id',
-      // verifyToken
+      // verifyToken,
       // verifyToken,
       // verifyAdmin,
       async (req, res) => {
